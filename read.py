6c9ff0e8725c1c13be419b4685a737539d7d65a9
Jovan <Jovan@DESKTOP-JTIACNO>
2020-12-12 17:12:23 +0100

    Fix:

--- read.py
+++ read.py
@@ -103,4 +103,4 @@ result = save_to_db(elements=db_elements)
 if result:
     print("Successfully inserted records to mongoDB")
 else:
-    print("Error while working with local mongoDB")
+    print("Error while working with localhost mongoDB")
